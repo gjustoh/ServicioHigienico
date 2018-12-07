9fdd8e2eecaf3a6b24571d6a5166197c8807804c
Gabriel Alex Justo Huayna <gjusto@unsa.edu.pe>
2018-12-07 03:13:33 -0500

    Version final de wrapper, listo para subir al articulo

--- wraper.c
+++ wraper.c
@@ -8,31 +8,52 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/sem.h>
+/*
+ *
+ * definimos la union que consta de diferentes tipos con la finalidad de crear varios semaforos e inicializarlos:
+ *  
+ */
 union semun {
-    int val;               //valor para SETVAL
-    struct semid_ds *buf;  //buffer para IPC_STAT, IPC_SET
-    unsigned short *array; //arreglo para GETALL, SETALL
-    struct seminfo *__buf; //buffer para IPC_INFO
+    int val;
+    struct semid_ds *buf;
+    unsigned short *array;
+    struct seminfo *__buf;
 };
-//declaraciones de las funciones wrappers para simplificarnos la vida
+
+/*
+ *
+ * Reservar memoria compartida
+ * n: tamaño para reservar en la memoria compartida
+ *  
+ */
+
 int ReservarMemoriaComp(int n)
 {
     return shmget(IPC_PRIVATE, n, IPC_CREAT | SHM_R | SHM_W);
 }
+
+/*
+ *
+ * Mapear memoria compartida donde:
+ * id: identificador para compartir la memoria entre varios procesos
+ * 
+ */
+
 void *MapearMemoriaComp(int id)
 {
     void *addr;
-    addr = shmat(id, NULL, 0);  //adjuntamos el segmento
-    //  shmctl(id, IPC_RMID, NULL); //y lo marcamos para que se destruya
+    addr = shmat(id, NULL, 0);
     return addr;
 }
-int ReservarMemoriaCompT(size_t size)
-{
-    return shmget(IPC_PRIVATE, size, IPC_CREAT | 0600);
-}
-int CrearSemaforo1(key_t key,int n){
-    semget(key, n, SHM_R | SHM_W);
-}
+
+/*
+ *
+ * Crear los grupos de semaforos donde:
+ * n: cantidad desemaforos para el grupo
+ * vals: inicializacion de cada semaforo
+ * 
+ */
+
 int CrearSemaforos(int n, short *vals)
 {
     union semun arg;
@@ -42,15 +63,12 @@ int CrearSemaforos(int n, short *vals)
     semctl(id, 0, SETALL, arg);
     return id;
 }
-int CrearSemaforos2(int n, short *vals)
-{
-    union semun arg;
-    int id;
-    id = semget(IPC_PRIVATE, n, SHM_R | SHM_W);
-    arg.array = vals;
-    semctl(id, 0, SETALL, arg);
-    return id;
-}
+/*
+ *
+ * borrar el grupos de semaforos mediante un identificador donde:
+ * id: identificador del grupo de semaforos para ser borrados
+ * 
+ */
 void BorrarSemaforos(int id)
 {
     if (semctl(id, 0, IPC_RMID, NULL) == -1)
@@ -59,6 +77,29 @@ void BorrarSemaforos(int id)
         exit(EXIT_FAILURE);
     }
 }
+/*
+ *
+ * borrar memoria compartida mediante un identificador donde:
+ * id: identificador de la memoria compartida
+ * 
+ */
+void BorrarMemoriaComp(int id)
+{
+    if (shmctl(id, IPC_RMID, NULL) == -1)
+    {
+        perror("Error liberando memoria!");
+        exit(EXIT_FAILURE);
+    }
+}
+
+/*
+ *
+ * bloquear un semaforo especifico donde:
+ * id: identificador del grupo de semaforos
+ * i: poscicion del semaforo en el arreglo para ser bloqueado
+ *  
+ */
+
 void BloquearSemaforo(int id, int i)
 {
     struct sembuf sb;
@@ -67,6 +108,13 @@ void BloquearSemaforo(int id, int i)
     sb.sem_flg = SEM_UNDO;
     semop(id, &sb, 1);
 }
+/*
+ *
+ * desbloquear un semaforo especifico donde:
+ * id: identificador del grupo de semaforos
+ * i: poscicion del semaforo en el arreglo para ser desbloqueado
+ *  
+ */
 void DesbloquearSemaforo(int id, int i)
 {
     struct sembuf sb;
@@ -75,9 +123,3 @@ void DesbloquearSemaforo(int id, int i)
     sb.sem_flg = SEM_UNDO;
     semop(id, &sb, 1);
 }
-
-enum
-{
-    SEM_USER_1, //el turno de la primera persona
-    SEM_USER_2  //el turno de la 2da persona
-};
